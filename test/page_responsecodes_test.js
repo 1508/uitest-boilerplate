2965c2259e3803225385b750e69bb7af41fbe80b
Sune Radich Vestergaard <sykokaj@gmail.com>
2016-10-07 11:36:23 +0200

    Remove demo logic and tests
    The boilerplate should be as clean as possible

--- test/page_responsecodes_test.js
+++ test/page_responsecodes_test.js
@@ -3,33 +3,11 @@
 
 Feature('Testing page response codes');
 
-//Require the mock Express server
-let server = require('./../server');
+BeforeSuite((I) => {});
 
-BeforeSuite((I) => { // or Background
-    server.start();
-});
-
-AfterSuite((I) => {
-    server.close();
-});
+AfterSuite((I) => {});
 
 Scenario('Response code 200', (I) => {
-
-    I.customAmOnPage('/page200');
-    I.getServerResponseCode(202);
-
-});
-
-Scenario('Response code 404', (I) => {
-
-    I.customAmOnPage('/page404');
-    I.getServerResponseCode(404);
-
-});
-
-Scenario('Response code 500', (I) => {
-
-    I.customAmOnPage('/page500');
-    I.getServerResponseCode(500);
+    I.customAmOnPage('/');
+    I.getServerResponseCode(200);
 });
